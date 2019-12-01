574f0ee33d79543974f07dafce368d5676d94445
Finnur Eiríksson <klauvi@gmail.com>
2019-12-01 22:07:56 +0000

    day1 part2

--- 01/index.js
+++ 01/index.js
@@ -9,3 +9,15 @@ const getFuelReq = mass => Math.floor(mass / 3) - 2;
 const part1 = values.split('\n').reduce((acc, value) => acc += getFuelReq(value), 0);
 
 console.log(part1);
+
+
+const part2 = values.split('\n').reduce((acc, value) => {
+    let fuel = getFuelReq(value);
+    acc += fuel;
+    while ((fuel = getFuelReq(fuel)) > 0) {
+        acc += fuel;
+    }
+    return acc;
+}, 0);
+
+console.log(part2);
