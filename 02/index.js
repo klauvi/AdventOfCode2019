1836cc039ee5cb325337ed5b0d0d5be02b96099a
Finnur Eiríksson <finnur@sensa.is>
2019-12-02 09:40:23 +0000

    day2 part2

--- 02/index.js
+++ 02/index.js
@@ -4,19 +4,33 @@ const readInput = (file = 'input') => fs.readFileSync(`${__dirname}/${file}.txt`
 
 const input = readInput();
 
-const ops = input.split(',').map(Number);
-
-let i = 0;
-
-while (true) {
-  const op = ops[i++];
-  if (op === 99) {
-    break;
+const run = (noun, verb) => {
+  const ops = input.split(',').map(Number);
+  ops[1] = noun;
+  ops[2] = verb;
+  let i = 0;
+  while (true) {
+    const op = ops[i++];
+    if (op === 99) {
+      break;
+    }
+    const pos1 = ops[i++];
+    const pos2 = ops[i++];
+    const res = ops[i++];
+    ops[res] = op === 1 ? (ops[res] = ops[pos1] + ops[pos2]) : ops[pos1] * ops[pos2];
   }
-  const pos1 = ops[i++];
-  const pos2 = ops[i++];
-  const res = ops[i++];
-  ops[res] = op === 1 ? (ops[res] = ops[pos1] + ops[pos2]) : ops[pos1] * ops[pos2];
-}
+  return ops[0];
+};
+console.log(run(12, 2));
 
-console.log(ops[0]);
+const expect = 19690720;
+
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    const result = run(i, j);
+    if (result === expect) {
+      console.log(i, j, 100 * i + j);
+      break;
+    }
+  }
+}
