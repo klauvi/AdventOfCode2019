49aefade2eb3fe546df28dfbaca63d116bfdde8f
Finnur Eiríksson <finnur@sensa.is>
2019-12-03 09:28:59 +0000

    day3 part2

--- 03/index.js
+++ 03/index.js
@@ -5,10 +5,12 @@ const readInput = (file = 'input') => fs.readFileSync(`${__dirname}/${file}.txt`
 const input = readInput();
 
 let shortest = Infinity;
+let stepcount = Infinity;
 
 const [line1, line2] = input.split('\n');
 
 const grid = {};
+const steps = {};
 const adders = {
   R: [1, 0],
   L: [-1, 0],
@@ -18,6 +20,7 @@ const adders = {
 
 let x = 0;
 let y = 0;
+let step = 0;
 
 line1.split(',').forEach(instr => {
   const dir = instr.slice(0, 1);
@@ -25,19 +28,28 @@ line1.split(',').forEach(instr => {
   [...Array(length).keys()].forEach(_ => {
     x += adders[dir][0];
     y += adders[dir][1];
-    grid[`${x};${y}`] = true;
+    step++;
+    const key = `${x};${y}`;
+    grid[key] = true;
+    steps[key] = steps[key] ? steps[key] : step;
   });
 });
 
 x = 0;
 y = 0;
+step = 0;
 line2.split(',').forEach(instr => {
   const dir = instr.slice(0, 1);
   const length = +instr.slice(1);
   [...Array(length).keys()].forEach(_ => {
     x += adders[dir][0];
     y += adders[dir][1];
-    if (grid[`${x};${y}`]) {
+    step++;
+    const key = `${x};${y}`;
+    if (grid[key]) {
+      if (stepcount > step + steps[key]) {
+        stepcount = step + steps[key];
+      }
       const current = Math.abs(x) + Math.abs(y);
       if (current < shortest) {
         shortest = current;
@@ -47,3 +59,4 @@ line2.split(',').forEach(instr => {
 });
 
 console.log(shortest);
+console.log(stepcount);
