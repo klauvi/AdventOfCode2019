1fe2c365f14e55928755bf33d7394eeea33aded0
Finnur Eiríksson <finnur@sensa.is>
2019-12-04 10:57:13 +0000

    day4 part2

--- 04/index.js
+++ 04/index.js
@@ -4,6 +4,7 @@ const higher = 585159;
 const check = str => {
   let ok = true;
   let double = false;
+  let onlydouble = false;
   str.split('').reduce((prev, current) => {
     if (current < prev) {
       ok = false;
@@ -13,7 +14,17 @@ const check = str => {
     }
     return current;
   }, 0);
-  return ok && double;
+  if (/00/.test(str) && !/000/.test(str)) onlydouble = true;
+  if (/11/.test(str) && !/111/.test(str)) onlydouble = true;
+  if (/22/.test(str) && !/222/.test(str)) onlydouble = true;
+  if (/33/.test(str) && !/333/.test(str)) onlydouble = true;
+  if (/44/.test(str) && !/444/.test(str)) onlydouble = true;
+  if (/55/.test(str) && !/555/.test(str)) onlydouble = true;
+  if (/66/.test(str) && !/666/.test(str)) onlydouble = true;
+  if (/77/.test(str) && !/777/.test(str)) onlydouble = true;
+  if (/88/.test(str) && !/888/.test(str)) onlydouble = true;
+  if (/99/.test(str) && !/999/.test(str)) onlydouble = true;
+  return ok && double && onlydouble;
 };
 
 let total = 0;
